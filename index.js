d013da3e21004417ce5242ae554be2b4d266a0f3
selma <selma@gmail.com>
2017-08-23 14:10:42 +0200

    rajout de la machine template

--- index.js
+++ index.js
@@ -1,10 +1,48 @@
-const express = require('express')
+const fs = require('fs');
+const express = require('express');
+const mustache = require('mustache');
+
 let app = express();
 
-// acceder au fichier dans le dossier public qu'on a partagé sur internet et qu'on a lancé
-// au serveur web 
-app.use(express.static("public"));
+app.get("/", function(req, resp) {
+    resp.render('index', {
+        name: 'Selma',
+        adjective: 'hungry'
+    });
+});
 
+app.get("/test", function(req, resp) {
+    let str = mustache.render("Hello {{name}}!!! You are awesome!", {
+        name: "Selma"
+    });
+    resp.send(str)
+});
+// creation de template engine a express
+app.engine("html", function(path, options, callback) {
+    // ouverture de fichier, on appelle une function quand on accede au fichier
+    fs.readFile(path, function(err, content) {
+
+        // on test si il y a l'erreur, on return function callback
+        if (err) {
+            console.error("fail to open template:", err);
+            return callback(err);
+        }
+        // si tout vas bien, le mustache prend le contenu de fichier et les options
+        let str = mustache.render(content.toString(), options);
+
+        // pas d'erreur, et on donne des donnees str
+        return callback(null, str);
+    })
+});
+
+let db = ["Toto", "Tata", "Titi", "John", "Tutu"];
+// specifier les directory
+app.set("views", "./template");
+
+// enregistrer la machine template
+app.set("view engine", "html");
+
+app.use(express.static("public"));
 // on a  lancer serveur web sur port 80
 app.listen(80, "localhost", function() {
     console.log('Server listening on port 80!');
